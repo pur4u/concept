3c6256c2e5336e9797387f176daa7abd1ee0646b
Ovidiu L <new2style@yahoo.com>
2015-12-08 23:50:51 +0200

    testcap

--- system/helpers/captcha_helper.php
+++ system/helpers/captcha_helper.php
@@ -94,12 +94,15 @@ if ( ! function_exists('create_captcha'))
 			}
 		}
 
+		print_r(extension_loaded('gd'));
 		if ($img_path === '' OR $img_url === ''
 			OR ! is_dir($img_path) OR ! is_really_writable($img_path)
 			OR ! extension_loaded('gd'))
 		{
+			print_r('false');
 			return FALSE;
 		}
+		print_r('bau');
 
 		// -----------------------------------
 		// Remove old images
@@ -108,7 +111,7 @@ if ( ! function_exists('create_captcha'))
 		$now = microtime(TRUE);
 
 		$current_dir = @opendir($img_path);
-		
+
 		while ($filename = @readdir($current_dir))
 		{
 			print_r($filename);
